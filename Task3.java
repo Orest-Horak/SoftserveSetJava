4bbac388e8e9a5b8783da36624e5cb1f604e342c
Horak Orest <orgo98@ukr.net>
2019-06-25 11:50:42 +0300

    update

--- Task3.java
+++ Task3.java
@@ -1,52 +1,34 @@
-package homeworkl8;
-
+package Task8;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Task3 {
 
-	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Pattern pattern = Pattern.compile("^\\$(\\d+)(\\.\\d{2})+");
-		System.out.println("Write how many currensy you have");
-		int c = 0;
-		try {
-			c = Integer.parseInt(br.readLine());
-		} catch (NumberFormatException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		List<String> currency = new ArrayList<String>();
-		for (int i = 0; i < c; i++) {
+	public static void main(String[] args) throws IOException {
+		System.out.println("Please enter the US currency ");
+		String pattern = "[$][0-9]+[.][0-9]{2}";
+		//	String pattern = "($)(\\d+)(.*)(\\d{2})";
 
-			System.out.println("Enter currency type $777.77 N " + i);
-			try {
-				currency.add(br.readLine());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		for (int i = 0; i < currency.size(); i++) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String text =reader.readLine();
 
-			Matcher m = pattern.matcher(currency.get(i));
+		//String text ="Orest k Petro wqe_rrq213434898 Ivan";
+		String[] q=text.split(" ");
+		Pattern p = Pattern.compile(pattern);
+		for(int i=0 ; i<q.length ; i++) {
+			Matcher m = p.matcher(q[i]);
 			if (m.matches()) {
-				System.out.println("All what you write is ok!YUPPI");
-			} else {
-				System.out.println("Ups smth is wrog, sorry ;)");
+				System.out.print(q[i]+" matches the US currency");
+				System.out.println();
 			}
-
-			m.reset();
-			while (m.find()) {
-				System.out.println(currency.get(i).substring(m.start(), m.end()));
+			else
+			{
+				System.out.println(q[i] + " Wrong input");
 			}
 		}
-
 	}
 
 }
